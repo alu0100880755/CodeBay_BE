aa715c0e4c4b92fea022e0f0511fac6a249b6688
Iván Josué Borges González <ivanbor8@gmail.com>
2020-10-08 22:19:45 +0100

    Configuration to connect and create the database automatically

--- src/main/java/com/init/users/entities/User.java
+++ src/main/java/com/init/users/entities/User.java
@@ -2,15 +2,42 @@ package com.init.users.entities;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
 	// Attributes
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+
+	@Column(name = "name", nullable = false, length = 30)
 	private String Name;
+
+	@Column(name = "surname", nullable = false, length = 30)
 	private String Surname;
+
+	@Column(name = "email", nullable = false, length = 30)
 	private String Email;
+
+	@Column(name = "city", nullable = false, length = 30)
 	private String City;
+
+	@Column(name = "active", nullable = false, length = 30)
 	private Boolean Active;
+
+	@Column(name = "birthday", nullable = false, length = 30)
 	private String Birthday;
+
+	@Column(name = "creation_date", nullable = false, length = 30)
 	private LocalDateTime Creation_Date;
 
 	// Getter & Setters
--- src/main/java/com/init/users/rest/UsersREST.java
+++ src/main/java/com/init/users/rest/UsersREST.java
@@ -1,5 +1,22 @@
 package com.init.users.rest;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.init.users.entities.User;
+
+@RestController
+@RequestMapping("users")
 public class UsersREST {
+
+	// @RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping
+	public ResponseEntity<List<User>> getUser() {
+		return null;
+	}
 
 }
